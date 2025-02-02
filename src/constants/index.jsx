31a70fb6501b9dcfb38ec7ecbcd4bf03008c3d93
arystudent-demo <aryanjjchympa@gmail.com>
2025-02-02 13:05:11 +0530

    comitted

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -14,9 +14,9 @@ import user6 from "../assets/profile-pictures/user6.jpg";
 
 export const navItems = [
   { label: "Features", href: "#" },
-  { label: "Workflow", href: "#" },
-  { label: "Pricing", href: "#" },
-  { label: "Testimonials", href: "#" },
+  { label: "Workflow", href: "#Workflow" },
+  { label: "Pricing", href: "#Pricing" },
+  { label: "Testimonials", href: "#Testimonials" },
 ];
 
 export const testimonials = [
